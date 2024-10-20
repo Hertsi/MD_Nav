58c80023cffc081346a3a66188d17a5665aa2b32
Hertsi <nico.hertolin@protonmail.com>
2024-10-21 01:02:32 +0300

    testattu

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen, SecondScreen } from './components/screens';
-import CustomNavigationBar from './components/customnavbar';
+import { HomeScreen, SecondScreen } from './screens/Screens';
+import CustomNavigationBar from './components/CustomAppBar';
 
 const Stack = createStackNavigator();
 
--- components/CustomAppBar.js
+++ components/CustomAppBar.js
@@ -1,23 +1,22 @@
 import { Appbar } from 'react-native-paper';
 
-
 export default function CustomNavigationBar({ navigation, route, back }) {
-    const title = "MD Nav Demo";
+  const title = route.name === 'HomeScreen' ? 'Welcome Home' : 'Second Screen'; 
+  const handleBackAction = () => {
+    navigation.goBack();
+  };
 
-    const handleBackAction = () => {
-        navigation.goBack();
-    };
+  const handleForwardAction = () => {
+    if (route.name === 'HomeScreen') {
+      navigation.navigate('SecondScreen');
+    }
+  };
 
-    const handleForwardAction = () => {
-        if (route.name === 'HomeScreen') {
-          navigation.navigate('SecondScreen');
-        }
-      };
-return (
+  return (
     <Appbar.Header>
-     {back && <Appbar.BackAction onPress={handleBackAction} />}
+      {back && <Appbar.BackAction onPress={handleBackAction} />}
       <Appbar.Content title={title} />
-      {route.name === 'HomeScreen' && ( 
+      {route.name === 'HomeScreen' && (
         <Appbar.Action icon="arrow-right" onPress={handleForwardAction} />
       )}
     </Appbar.Header>
